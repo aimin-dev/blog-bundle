a903106b4e344f3f588f7675a89029b4f8889697
stephan <m6ahenina@gmail.com>
2020-05-06 22:26:04 +0300

    Fix typo

--- AiminBlogBundle.php
+++ AiminBlogBundle.php
@@ -4,13 +4,13 @@
  * @Author: stephan <m6ahenina@gmail.com>
  * @Date: 2020-05-06 19:24:19
  * @Last Modified by: stephan <m6ahenina@gmail.com>
- * @Last Modified time: 2020-05-06 19:32:27
+ * @Last Modified time: 2020-05-06 22:25:46
  */
 
 namespace Aimin\Bundle\BlogBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class AimingBlogBundle extends Bundle
+class AiminBlogBundle extends Bundle
 {
 }
